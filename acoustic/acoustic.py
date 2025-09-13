c9c141dbe0876ede37199e7c6782a8f4149ce3f8
Lambda <61808223+L-mbda@users.noreply.github.com>
2025-09-12 20:15:16 -0700

    Model freezing and understanding LoRA

--- acoustic/acoustic.py
+++ acoustic/acoustic.py
@@ -1,3 +1,22 @@
 from transformers import WavLMModel
+import torch, peft, gputil
 
-model = WavLMModel.from_pretrained("microsoft/wavlm-base")
+WAVLM_Model = WavLMModel.from_pretrained("microsoft/wavlm-base")
+
+# Freeze wavlm
+for i in WAVLM_Model.parameters():
+    i.requires_grad = False
+
+# LoRA
+LORA_Config = peft.LoraConfig(
+    r=8,
+    lora_alpha=16,
+    target_modules=["q_proj", "k_proj"],
+    bias="none",
+    task_type=peft.TaskType.FEATURE_EXTRACTION,
+    layers_to_transform=list(range(len(WAVLM_Model.encoder.layers) - 4, len(WAVLM_Model.encoder.layers))),
+    layers_pattern="layers"
+)
+
+LORA_Model = peft.get_peft_model(WAVLM_Model, LORA_Config)
+LORA_Model.print_trainable_parameters()
